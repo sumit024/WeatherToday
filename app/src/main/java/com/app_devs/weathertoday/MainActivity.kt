4e39d1fceca2c3b2b4b2923c111d6faa0df7f9a4
Sumit Kushwaha <kushwahasumit024@gmail.com>
2021-08-01 10:57:59 +0530

    UI set up

--- app/src/main/java/com/app_devs/weathertoday/MainActivity.kt
+++ app/src/main/java/com/app_devs/weathertoday/MainActivity.kt
@@ -2,18 +2,23 @@ package com.app_devs.weathertoday
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.icu.util.TimeZone
 import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import com.app_devs.weathertoday.models.WeatherResponse
 import com.app_devs.weathertoday.network.WeatherService
@@ -24,13 +29,17 @@ import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit.GsonConverterFactory
 import retrofit.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mFusedLocationClient:FusedLocationProviderClient
     private var mLatitude:Double=0.0
     private var mLongitude:Double=0.0
+
+    private var mProgressDialog:Dialog?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -156,12 +165,18 @@ class MainActivity : AppCompatActivity() {
             val listCall: Call<WeatherResponse> = service.getWeather(
                     latitude, longitude, Constants.APP_ID, Constants.METRIC_UNIT)
 
+            showCustomDialog()
+
             listCall.enqueue(object : Callback<WeatherResponse>{
+                @RequiresApi(Build.VERSION_CODES.N)
                 override fun onResponse(response: Response<WeatherResponse>?, retrofit: Retrofit?) {
                     if(response!!.isSuccess)
                     {
+
                         val weatherList:WeatherResponse=response.body()
+                        setUpUI(weatherList)
                         Log.i("Response","$weatherList")
+                        hideProgressDialog()
                     }
                     else
                     {
@@ -178,6 +193,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onFailure(t: Throwable?) {
+                    hideProgressDialog()
                     Log.e("Errorrr",t!!.message.toString())
                 }
 
@@ -187,6 +203,78 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"Not Connected",Toast.LENGTH_SHORT).show()
 
     }
+
+    private fun showCustomDialog()
+    {
+        mProgressDialog= Dialog(this)
+        mProgressDialog!!.setContentView(R.layout.dialog_custom_progress)
+        mProgressDialog!!.show()
+    }
+    private fun hideProgressDialog()
+    {
+        mProgressDialog?.dismiss()
+    }
+
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(Build.VERSION_CODES.N)
+
+    private fun setUpUI(weatherList: WeatherResponse)
+    {
+        for(i in weatherList.weather.indices)
+        {
+            tv_main.text=weatherList.weather[i].main
+            tv_main_description.text=weatherList.weather[i].description
+            tv_temp.text=weatherList.main.temp.toString()+  getUnit(application.resources.configuration.locales.toString())
+
+            tv_sunrise_time.text=unixTime(weatherList.sys.sunrise.toLong())
+            tv_sunset_time.text=unixTime(weatherList.sys.sunset.toLong())
+
+            tv_humidity.text = weatherList.main.humidity.toString() + "%"
+            tv_min.text = weatherList.main.temp_min.toString() + " min"
+            tv_max.text = weatherList.main.temp_max.toString() + " max"
+            tv_speed.text = weatherList.wind.speed.toString()
+            tv_name.text = weatherList.name
+            tv_country.text = weatherList.sys.country
+
+            when(weatherList.weather[i].icon)
+            {
+                "01d"-> iv_main.setImageResource(R.drawable.sunny)
+                "02d" -> iv_main.setImageResource(R.drawable.cloud)
+                "03d" -> iv_main.setImageResource(R.drawable.cloud)
+                "04d" -> iv_main.setImageResource(R.drawable.cloud)
+                "04n" -> iv_main.setImageResource(R.drawable.cloud)
+                "10d" -> iv_main.setImageResource(R.drawable.rain)
+                "11d" -> iv_main.setImageResource(R.drawable.storm)
+                "13d" -> iv_main.setImageResource(R.drawable.snowflake)
+                "01n" -> iv_main.setImageResource(R.drawable.cloud)
+                "02n" -> iv_main.setImageResource(R.drawable.cloud)
+                "03n" -> iv_main.setImageResource(R.drawable.cloud)
+                "10n" -> iv_main.setImageResource(R.drawable.cloud)
+                "11n" -> iv_main.setImageResource(R.drawable.rain)
+                "13n" -> iv_main.setImageResource(R.drawable.snowflake)
+            }
+        }
+    }
+    private fun getUnit(value:String): String
+    {
+        Log.i("unitttttt", value)
+        var value = "°C"
+        if ("US" == value || "LR" == value || "MM" == value) {
+            value = "°F"
+        }
+        return value
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun unixTime(timex:Long):String
+    {
+        val date=Date(timex*1000L)
+        val sdf= SimpleDateFormat("HH:mm",Locale.UK)
+        sdf.timeZone= TimeZone.getDefault()
+        return sdf.format(date)
+    }
+
 
 
 
